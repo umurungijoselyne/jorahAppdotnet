65f5aab5b2dabc1c809a39de1b8a60c882044064
niyivugukuri12 <jniyivugukuri28@gmail.com>
2019-02-25 07:18:25 +0200

    first commit

---
+++ Model.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace ConsoleApp2.Db
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movietbl> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;");
+        }
+    }
+
+    public class Movietbl
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string title { get; set; }
+        public string year { get; set; }
+        public string category_name { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2.Db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext()) 
+            {
+                Console.WriteLine("enter movie title");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter movie realised year");
+                string year = Console.ReadLine();
+                Console.WriteLine("enter movie category_name");
+                string category_name = Console.ReadLine();
+                db.Movies.Add(new Movietbl { title = "vimpire d'iarises" });
+                db.Movies.Add(new Movietbl { year = "2019" });
+                db.Movies.Add(new Movietbl { category_name = "vimpire d'iarises" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All Movies in database:");
+                foreach (var Movietbl in db.Movies)
+                {
+                    Console.WriteLine(" - {0} {1} {2}", Movietbl.title, Movietbl.year, Movietbl.category_name);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
